e827a93320b418da1fa45b0e5c47c4658979b2db
Acturus2020 <milestoneblood@gmail.com>
2021-06-15 10:50:52 +0300

    create create and delete buttons

--- main/views.py
+++ main/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Pizza
 from django.views.generic import TemplateView
+import requests
 # Create your views here.
 
 
 def index(request):
+    # print(kwa)
     Pizzas = Pizza.objects.all()
     return render(request, 'main/index.html', {'Pizzas': Pizzas})
+
 
 
 def sorted(request):
@@ -21,10 +23,30 @@ def filtered(request):
 
 
 class BasePizzaTemplate(TemplateView):
+
     def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['pizzas'] = Pizza.objects.all()
         create_pizza = self.request.GET.get('create_pizza') == "yes"
         if create_pizza:
             Pizza.objects.create(
                 name='new pizza',
                 price=123
             )
+        delete_pizza = self.request.GET.get('delete_pizza') == "yes"
+        if delete_pizza:
+            del_pizza = Pizza.objects.all().order_by('?').first()
+            del_pizza.delete()
+        return context
+
+class PizzaView(BasePizzaTemplate):
+    template_name = "main/index.html"
+
+
+class PizzaDetailView(TemplateView):
+    template_name = "main/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['pizza'] = Pizza.objects.get(id=kwargs.get('pizza_id'))
+        return context
